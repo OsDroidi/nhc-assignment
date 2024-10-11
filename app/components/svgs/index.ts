875b7eb5e34c1c558088a6d0414623853ed31162
OsDroidi <osdroidi@gmail.com>
2024-10-11 19:58:05 +0300

    icons

---
+++ app/components/svgs/index.ts
@@ -0,0 +1,6 @@
+export { default as NHCLogo } from './NHCLogo.svg';
+export { default as Vision2030 } from './Vision2030.svg';
+export { default as GrayStar } from './GrayStar.svg';
+export { default as Star } from './Star.svg';
+export { default as SearchIcon } from './SearchIcon.svg';
+export { default as Empty } from './Empty.svg';
