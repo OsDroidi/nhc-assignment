857ded6e5e6a9c23bf76ef4ff56fc17efdf70e90
OsDroidi <osdroidi@gmail.com>
2024-10-11 19:56:50 +0300

    install google fonts projects configuration

---
+++ app/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Abel } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-inter',
+});
+
+export const abel = Abel({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+  variable: '--font-abel',
+});
